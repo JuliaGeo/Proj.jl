61e6c029ecc6b6992802e2d7f6b022ccb4cd8879
Chris Foster <chris42f@gmail.com>
2015-08-27 23:45:10 +1000

    Very basic test case for sanity check

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using Proj4
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Some very basic sanity checking
+wgs84 = Projection("+proj=longlat +datum=WGS84 +no_defs")
+utm56 = Projection("+proj=utm +zone=56 +south +datum=WGS84 +units=m +no_defs")
+
+# Reference data computed using GeographicLib's GeoConvert tool
+@test maximum(transform(wgs84, utm56, [150 -27 0]) .-
+              [202273.912995055 7010024.033113679 0]) < 1e-6
+
