ceca689aee3b88f99a32443f2222fbbc47864b4c
Chris Foster <chris42f@gmail.com>
2015-08-28 22:58:00 +1000

    Implement print(::Projection) and shorten show()
    This should make string(::Projection) work correctly.  Also remove the
    doc string for show(), since the functionality for this interface is
    defined in Base, not here.

--- src/Proj4.jl
+++ src/Proj4.jl
@@ -80,15 +80,9 @@ function Projection(proj_string::String)
     proj
 end
 
-
-"""
-Show a projection in human readable form
-"""
-function Base.show(io::IO, proj::Projection)
-    defstr = strip(_get_def(proj))
-    print(io, "Projection(\"$defstr\")")
-end
-
+# Pretty printing
+Base.print(io::IO, proj::Projection) = print(io, strip(_get_def(proj)))
+Base.show(io::IO, proj::Projection) = print(io, "Projection(\"$proj\")")
 
 """
 Return true if the projection is a geographic coordinate system (lon,lat)
