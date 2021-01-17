a7321c9f0e3860f2b4d993a0a1da0a24374e7f45
Martijn Visser <mgvisser@gmail.com>
2021-01-17 01:19:45 +0100

    make Coord an alias of SVector{4, Float64} instead
    This backtracks #50 mostly.
    
    It doesn't quite feel right to name the fields x and y even though the axis order is up to the CRS by default.
    
    In [the PROJ docs](https://proj.org/development/reference/functions.html?highlight=coordinate%20components#c.proj_trans_generic) they had to include this note for instance:
    
    > Even though he coordinate components are named x, y, z and t, axis ordering of the to and from CRS is respected. Transformations exhibit the same behavior as if they were gathered in a PJ_COORD struct.
    
    (cherry picked from commit 1d0c03ef93701a2f4a2d36d8ea3eea0238e152d1)

--- src/coord.jl
+++ src/coord.jl
@@ -1,17 +1,1 @@
-"""
-    Coord <: FieldVector{4, Float64}
-
-General purpose coordinate type, applicable in two, three and four dimensions. This is the
-default coordinate datatype used in PROJ.
-
-Elements can be retrieved either by index 1-4, or by field x, y, z, t. If a Coord does not
-represent a cartesian coordinate, using the index may be more clear, as the other coordinate
-types listed in the [PJ_COORD docs](https://proj.org/development/reference/datatypes.html#c.PJ_COORD)
-are not addressable by name.
-"""
-struct Coord <: FieldVector{4, Float64}
-    x::Float64
-    y::Float64
-    z::Float64
-    t::Float64
-end
+const Coord = SVector{4, Float64}
